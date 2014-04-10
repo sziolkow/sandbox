01c8493ff4041e64056fcf6a66c3e5120acc6699
slawomir.ziolkowski <sziolkow@gmail.com>
2014-04-10 10:18:50 +0200

    Add method done with tests

--- code-jam/CustomCalculator/src/main/java/Calculator/CustomCalculator/impl/Calculator.java
+++ code-jam/CustomCalculator/src/main/java/Calculator/CustomCalculator/impl/Calculator.java
@@ -9,13 +9,29 @@ public class Calculator implements CCalculator {
 		String longString = arg2;
 		if(shortString.length()>longString.length()) {
 			shortString = arg2;
-			longString = arg2;
+			longString = arg1;
 		}
-		for (int i = 1; i < shortString.length(); i++) {
+		
+		StringBuffer result = new StringBuffer();
+		int transposition = 0;
+		for (int i = 1; i <= shortString.length(); i++) {
 			char a = shortString.charAt(shortString.length() - i);
+			char b = longString.charAt(longString.length() - i);
+			int partResult = Character.getNumericValue(a) +Character.getNumericValue(b) + transposition;
+			transposition = 0;
+			if (partResult >= 10) {
+				partResult = partResult -10;
+				transposition = 1;
+			}
+			
+			result.append(partResult);
 		}
-		‚
-		return null;
+		
+		if(longString.length()-shortString.length()>0) {
+			result.append(longString.subSequence(0,longString.length() - shortString.length()));
+		}
+		
+		return result.reverse().toString();
 	}
 
 	public String multiply(String arg1, String arg2) {
--- code-jam/CustomCalculator/src/test/java/Calculator/CustomCalculator/impl/CalculatorTest.java
+++ code-jam/CustomCalculator/src/test/java/Calculator/CustomCalculator/impl/CalculatorTest.java
@@ -11,9 +11,31 @@ public class CalculatorTest {
 	CCalculator calculator = new Calculator();
 	
 	@Test
-	public void addTwoSimplyStrings() {
-		String result = calculator.add("10", "10");
-		assertEquals("20", result);
+	public void addTwoStringsSameLength() {
+		String result = calculator.add("410", "310");
+		System.out.println(result);
+		assertEquals("720", result);
+	}
+
+	@Test
+	public void addTwoStringsDifferentLength() {
+		String result = calculator.add("1310", "310");
+		System.out.println(result);
+		assertEquals("1620", result);
+	}
+
+	@Test
+	public void addTwoStringsSameLengthwithTransposition() {
+		String result = calculator.add("450", "350");
+		System.out.println(result);
+		assertEquals("800", result);
+	}
+	
+	@Test
+	public void addTwoStringsDifferentLengthwithTransposition() {
+		String result = calculator.add("1450", "350");
+		System.out.println(result);
+		assertEquals("1800", result);
 	}
 
 }
