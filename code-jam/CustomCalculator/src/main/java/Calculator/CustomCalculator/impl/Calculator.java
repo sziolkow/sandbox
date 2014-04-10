8a7511c4742570f5130286e4469fa7033529b768
Slawek <sziolkow@gmail.com>
2014-04-10 07:50:31 +0200

    Initial commit from eclipse

--- code-jam/CustomCalculator/src/main/java/Calculator/CustomCalculator/impl/Calculator.java
+++ code-jam/CustomCalculator/src/main/java/Calculator/CustomCalculator/impl/Calculator.java
@@ -5,7 +5,16 @@ import Calculator.CustomCalculator.CCalculator;
 public class Calculator implements CCalculator {
 
 	public String add(String arg1, String arg2) {
-		// TODO Auto-generated method stub
+		String shortString =arg1;
+		String longString = arg2;
+		if(shortString.length()>longString.length()) {
+			shortString = arg2;
+			longString = arg2;
+		}
+		for (int i = 1; i < shortString.length(); i++) {
+			char a = shortString.charAt(shortString.length() - i);
+		}
+		‚
 		return null;
 	}
 
